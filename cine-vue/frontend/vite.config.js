55eb72bca87eb44a2e202bcdf0cbe0e4b2ac048e
MrEdgarr <nqhuy11101@gmail.com>
2026-04-14 10:24:17 +0700

    Add plugin + DaisyUI: - unplugin-auto-import - unplugin-vue-components Config Vite

--- cine-vue/frontend/vite.config.js
+++ cine-vue/frontend/vite.config.js
@@ -1,18 +1,51 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+//plugin
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+//tailwind
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
+    AutoImport({
+      // Tự động import các hàm của vue, vue-router, pinia...
+      imports: ["vue", "vue-router", "pinia"],
+      dts: "src/auto-imports.d.ts", // Tạo file định nghĩa type tự động (cho TypeScript)
+    }),
+    Components({
+      // Tự động import component trong thư mục src/components
+      dirs: ["src/components"],
+      deep: true,
+      extensions: ["vue", "svg"],
+      include: [/\.vue$/, /\.vue\?vue/, /\.svg$/],
+      dts: "src/components.d.ts", // Tạo file định nghĩa type cho component
+      // directoryAsNamespace: false,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  build: {
+    target: "es2022", // hoặc 'esnext' → bỏ code legacy, build nhanh hơn
+    minify: "esbuild", // Rolldown dùng Oxc/esbuild minifier cực nhanh
+    cssMinify: "lightningcss", // mặc định trong Vite 8, nhanh hơn PostCSS
+    reportCompressedSize: false, // tắt nếu không cần, tiết kiệm ~0.5-1s
+    chunkSizeWarningLimit: 1000, // tránh warning không cần thiết
+  },
+  // Tùy chọn nâng cao (nếu dự án lớn dần)
+  optimizeDeps: {
+    include: ["vue"], // pre-bundle nhanh hơn
+  },
+});
